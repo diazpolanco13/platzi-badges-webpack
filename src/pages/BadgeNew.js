2239b9a1459b441edbb18087b4c974b41df29f5e
Carlos Diaz <diazpolanco13@gmail.com>
2020-07-22 22:49:04 -0400

    Mi Primera pagina

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import './styles/BadgeNew.css';
-import header from '../images/badge-header.svg';
-import Navbar from '../components/Navbar';
-import Badge from '../components/Badge';
+import "./styles/BadgeNew.css";
+import header from "../images/badge-header.svg";
+import Navbar from "../components/Navbar";
+import Badge from "../components/Badge";
 
 class BadgeNew extends React.Component {
   render() {
     return (
       <div>
+        {/* Barra de navegacion */}
         <Navbar />
+        {/* Componente hero */}
         <div className="BadgeNew__hero">
           <img className="img-fluid" src={header} alt="Logo" />
         </div>
-
+        {/* Badge del usuario */}
         <div className="container">
           <div className="row">
             <div className="col">
               <Badge
-                firstName="Richard"
-                lastName="Kaufman"
-                twitter="sparragus"
-                jobTitle="Frontend Engineer"
-                avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+                firstName="Carlos"
+                lastName="Diaz"
+                twitter="diazpolanco13"
+                jobTitle="Estudiante de Platzi"
+                avatarUrl="https://en.gravatar.com/userimage/117147537/57b9084d31410d1a358b8e0318b19b84?size=200"
               />
             </div>
           </div>
